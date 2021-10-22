f74e81b9d3a49ad8490abc605262942e83818862
Bradley Playdon <61969883+bradleyplaydon@users.noreply.github.com>
2021-10-22 14:19:23 +0100

    Added vehicle search to showroom

--- showroom/views.py
+++ showroom/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from .models import Car
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+
+from .models import Car, Manufacturer, Model
 
 # Create your views here.
 
@@ -7,10 +11,43 @@ from .models import Car
 def showroom(request):
 
     cars = Car.objects.all()
+    manufacturers = Manufacturer.objects.all()
+    models = Model.objects.all()
+    query = None
+    sort = None
+    direction = None
+    
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                cars = cars.annotate(lower_name=Lower('name'))
+            if sortkey == 'model':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            cars = cars.order_by(sortkey)
 
-    template = 'showroom/showroom.html'
+        if 'manufacturer' in request.GET:
+            manufacturer = request.GET['manufacturer']
+            cars = Car.objects.filter(manufacturer__name=manufacturer)
+ 
+
+    current_sorting = f'{sort}_{direction}'
+    print(manufacturers)
     context = {
         'cars_for_sale': cars,
+        'current_manufacturer': manufacturer,
+        'manufacturers': manufacturers,
+        'search_term': query,
+        'models': models,
+        'current_sorting': current_sorting,
     }
+
+    template = 'showroom/showroom.html'
     
     return render(request, template, context)
