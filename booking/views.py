32dfe5308dabebe6035a423eea794af78a737d1a
ronin brad <bplaydon@gmail.com>
2021-10-26 21:28:56 +0100

    Added basic book in view & template

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 
+from .models import ServiceCar, Manufacturer, Model
 # Create your views here.
 
 def book_in(request):
-    return render(request, 'booking/book-in.html') 
+    service_cars = ServiceCar.objects.filter(do_service=True)
+    manufacturers = Manufacturer.objects.all()
+    models = Model.objects.all()
+
+    context = {
+        'service_cars': service_cars,
+        'manufacturers': manufacturers,
+        'models': models,
+    }
+
+    return render(request, 'booking/book-in.html', context) 
