39469a8668ead45e0e52e96ba5b72af432656400
ronin brad <bplaydon@gmail.com>
2021-10-27 22:12:37 +0100

    Added redirect to checkout

--- booking/views.py
+++ booking/views.py
@@ -5,11 +5,21 @@ from .models import ServiceCar, Manufacturer, Model
 # Create your views here.
 
 def book_in(request):
+   if request.method == "POST":
+        form_data = {
+            "date": request.POST.get('date'),
+            "manufacturer": request.POST.get('manufacturer'),
+            "model": request.POST.get('model'),
+            "service_type": request.POST.get('service_type')
+        }
+
+        return redirect('checkout')
+   else:
 
     service_cars = ServiceCar.objects.filter(do_service=True)
     manufacturers = Manufacturer.objects.all()
     models = Model.objects.all().order_by('name')
-
+        
     context = {
         'service_cars': service_cars,
         'manufacturers': manufacturers,
--- jp_performance_cars/settings.py
+++ jp_performance_cars/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'home',
     'showroom',
     'booking',
+    'checkout'
 ]
 
 MIDDLEWARE = [
