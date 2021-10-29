2e58aa281a62179d4c11abe4b8e08daac2465a4d
ronin brad <bplaydon@gmail.com>
2021-10-29 23:55:31 +0100

    Added condition if no booking request

--- booking/contexts.py
+++ booking/contexts.py
@@ -4,13 +4,14 @@ from .models import ServiceCar
 def booking_details(request):
 
     deposit_cost = 0
+    car = ""
     booking_req = request.session.get('booking_request', {})
-    car = get_object_or_404(ServiceCar, pk=booking_req["car_id"])
-
-    if booking_req["service_type"] == "annual":
-        deposit_cost += float(car.annual_service_price) - float(car.annual_service_price) * .90
-    else:
-        deposit_cost += float(car.minor_service_price) - float(car.minor_service_price) * .90
+    if "car_id" in booking_req:
+        car = get_object_or_404(ServiceCar, pk=booking_req["car_id"])
+        if booking_req["service_type"] == "annual":
+            deposit_cost += float(car.annual_service_price) - float(car.annual_service_price) * .90
+        elif booking_req["service_type"] == "minor":
+            deposit_cost += float(car.minor_service_price) - float(car.minor_service_price) * .90
 
     context = {
         'car': car,
