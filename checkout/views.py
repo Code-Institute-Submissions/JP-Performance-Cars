2efed2bd4ce2bec6395936e37c2d7126a80017f1
ronin brad <bplaydon@gmail.com>
2021-10-30 00:16:02 +0100

    Added basic view for checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.conf import settings
+from django.contrib import messages
+from booking.contexts import booking_details
+
+from .forms import BookingForm
+from .models import Booking
+
+import stripe
 
 # Create your views here.
 def checkout(request):
-    return render(request, 'checkout/checkout.html')
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    if request.method == "POST":
+       booking = request.session.get('booking_request', {})
+    else:
+        booking = request.session.get('booking_request', {})
+        if not booking:
+            messages.error(request, "Please create a booking first.")
+            return redirect(reverse('book_in'))
+
+        current_booking = booking_details(request)
+        deposit_cost = current_booking['deposit_cost']
+        stripe_total = round(deposit_cost * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+        booking_form = BookingForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
+    template = 'checkout/checkout.html'
+    context = {
+        'booking_form': booking_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
