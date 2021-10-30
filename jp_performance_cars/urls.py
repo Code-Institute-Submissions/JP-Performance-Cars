a9081ef311410791d6db52aad6963a9953a252da
ronin brad <bplaydon@gmail.com>
2021-10-30 17:20:08 +0100

    Fix unexpected indent for heroku

--- jp_performance_cars/urls.py
+++ jp_performance_cars/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -25,4 +26,5 @@ urlpatterns = [
     path('showroom/', include('showroom.urls')),
     path('book-in/', include('booking.urls')),
     path('checkout/', include('checkout.urls')),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
