671666227dd0b8d67df0a0ebe7ca5616a812bfb0
ronin brad <bplaydon@gmail.com>
2021-10-30 11:14:07 +0100

    Added service type to contexts.py

--- booking/contexts.py
+++ booking/contexts.py
@@ -5,9 +5,12 @@ def booking_details(request):
 
     deposit_cost = 0
     car = ""
+    service_type = ""
+
     booking_req = request.session.get('booking_request', {})
     if "car_id" in booking_req:
         car = get_object_or_404(ServiceCar, pk=booking_req["car_id"])
+        service_type = booking_req["service_type"]
         if booking_req["service_type"] == "annual":
             deposit_cost += float(car.annual_service_price) - float(car.annual_service_price) * .90
         elif booking_req["service_type"] == "minor":
@@ -16,6 +19,7 @@ def booking_details(request):
     context = {
         'car': car,
         'deposit_cost': deposit_cost,
+        'service_type': service_type,
     }
 
     return context
